d4c06ebdb6f26eb94d49ca9abf2c82ae3615a76e
Chris Nassif <chrisjnassif@gmail.com>
2025-09-01 14:14:53 -0400

    refactored and reorganized the test bench

--- alu_test_bench.sv
+++ alu_test_bench.sv
@@ -6,45 +6,61 @@ module alu_test_bench();
 
     logic clock;
     logic reset;
-    logic [2:0] opcode;
-    logic [7:0] a_in, b_in;
-    logic [7:0] c_out;
+    logic [2:0] alu_opcode;
+    logic [7:0] alu_input1, alu_input2;
+    logic [7:0] alu_output;
     
     
-    alu main_alu(.clock_in(clock), .reset_in(reset), .enable_in(1'b1), .opcode_in(opcode), .alu_input1(a_in), .alu_input2(b_in), .alu_output(c_out));
+    alu main_alu(
+        .clock_in(clock), .reset_in(reset), .enable_in(1'b1), 
+        .opcode_in(alu_opcode), .alu_input1(alu_input1), .alu_input2(alu_input2), 
+        .alu_output(alu_output)
+    );
 
     always begin
         #5 clock = !clock;
     end
  
-    //initial block...this is our test simulation
+
+
     initial begin
         $dumpfile("alu_test_bench.vcd"); //file to store value change dump (vcd)
         $dumpvars(0, alu_test_bench); //store everything at the current level and below
 
 
-        clock = 0; //0 is generally a safe value to initialize with and not specify size
+        clock = 0;
         reset = 0;
-        a_in = 0;
-        b_in = 0;
+        alu_input1 = 0;
+        alu_input2 = 0;
 
-        #10 reset = 1; //always good to reset
+        #10 reset = 1; // reset cycle
         #10 reset = 0;
         
-        $display("a_in     b_in     opcode  c_out");
+        // header
+        $display("  alu_input1    alu_input2   opcode       alu_output");
 
-        for (integer i=0; i<2; i++) begin
-          for (integer j=0; j<2; j++) begin
-            a_in = i;
-            b_in = j;
-            opcode = 3'b0;
+        // test addition opcode
+        for (integer i=10; i<20; i++) begin
+          for (integer j=15; j<20; j++) begin
+            alu_input1 = i;
+            alu_input2 = j;
+            alu_opcode = 3'b0;
             
             #30;
-            $display("%8b %8b %3b     %8b",a_in, b_in, opcode, c_out); //print values C-style formatting
-          
+            $display("%d           %d            %3b        %d",alu_input1, alu_input2, alu_opcode, alu_output); //print values C-style formatting
+
+            if (alu_output !== alu_input1 + alu_input2) begin
+                $error("Mismatch: Expected %h, Got %h at time %0t", alu_input1 + alu_input2, alu_output, $time);
+            end
+
           end
         end
         
+
+
+
+
+
         $display("Finishing Sim"); //print nice message at end
         $finish;
     end
